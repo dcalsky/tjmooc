1e2581030cb427439f4880422bfe293540934f76
dcalsky <dcalsky@163.com>
2017-01-26 22:16:49 +0800

    update setting: allowed_hosts, time_zone, language_code

--- tjmooc/tjmooc/settings.py
+++ tjmooc/tjmooc/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'yncdvv5i)t^k&qa9yy4*=0fv6j(7_2ty_4fxf&i%5ur=o#n3+t'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -110,9 +110,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.10/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-Hans'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
