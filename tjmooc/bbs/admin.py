2801e16fea9644a3d1c6149f9f648d7ae50dbb72
dcalsky <dcalsky@163.com>
2017-01-30 12:21:45 +0800

    add forum

--- tjmooc/bbs/admin.py
+++ tjmooc/bbs/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Forum, Post
+
+
+class ForumAdmin(admin.ModelAdmin):
+    list_display = ['name', 'course', 'moderator']
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'forum', 'user']
+
+
+admin.site.register(Forum, ForumAdmin)
+admin.site.register(Post, PostAdmin)
