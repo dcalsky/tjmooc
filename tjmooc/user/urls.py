900c026c7dc4c1e71f7b006d14f47637158664ec
dcalsky <dcalsky@163.com>
2017-02-01 21:13:10 +0800

    add rest view to Account

--- tjmooc/user/urls.py
+++ tjmooc/user/urls.py
@@ -1,7 +1,20 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
 from .views import new, show
+from rest_framework import routers, serializers, viewsets
+from .models import Account
+from .serializers import AccountSerializer
+
+
+class AccountViewSet(viewsets.ModelViewSet):
+    queryset = Account.objects.all()
+    serializer_class = AccountSerializer
+
+router = routers.DefaultRouter()
+router.register(r'account', AccountViewSet)
 
 urlpatterns = [
-    url(r'^$', show, name='show'),
-    url(r'^registe$r', new, name='register')
+    url(r'^', include(router.urls)),
+    # url(r'^$', show, name='show'),
+    url(r'^register$', new, name='register'),
+    url(r'^api-auth$', include('rest_framework.urls', namespace='rest_framework'))
 ]
