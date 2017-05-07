802d5076b0fb4ef0d7ce83cc99a2d0263a099a2e
dcalsky <dcalsky@163.com>
2017-05-07 15:59:20 +0800

    bbs: add get posts by forum id

--- tjmooc/bbs/urls.py
+++ tjmooc/bbs/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     url(r'^forums$', views.ForumList.as_view()),
     url(r'^forums/(?P<id>\w+)$', views.ForumDetail.as_view()),
-    url(r'^posts/(?P<id>\w+)$', views.PostDetail.as_view()),
-    url(r'^posts$', views.PostList.as_view())
+    url(r'^forums/(?P<forum_id>\w+)/posts$', views.PostList.as_view()),
+    url(r'^posts/(?P<id>\w+)$', views.PostDetail.as_view())
 ]
--- tjmooc/bbs/views.py
+++ tjmooc/bbs/views.py
@@ -1,12 +1,10 @@
 from django.conf import settings
-from rest_framework.generics import RetrieveAPIView, RetrieveUpdateDestroyAPIView, ListAPIView, ListCreateAPIView
+from rest_framework.generics import ListAPIView, RetrieveAPIView, RetrieveUpdateDestroyAPIView, GenericAPIView
 from rest_framework.permissions import IsAuthenticated, AllowAny
-from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
 from rest_framework import status
-from rest_framework.mixins import CreateModelMixin, ListModelMixin
 from rest_framework.views import APIView
-
+from jwt import decode, DecodeError
 from .permissions import IsOwnerOrReadOnly
 from .models import Forum, Post
 from .serializers import ForumSerializer, PostSerializer
@@ -26,18 +24,16 @@ class ForumDetail(RetrieveAPIView):
     queryset = Forum.objects.all()
     lookup_field = 'id'
 
-
 class PostList(APIView):
     permission_classes = (AllowAny,)
     serializer_class = PostSerializer
-    queryset = Post.objects.all()
 
-    def list(self, request, *args, **kwargs):
-        posts = self.paginate_queryset(self.get_queryset())
+    def get(self, request, format=None):
+        posts = Post.objects.all()
         serializer = PostSerializer(posts, many=True)
-        return self.get_paginated_response(serializer.data)
+        return Response(data=serializer.data, status=status.HTTP_200_OK)
 
-    def create(self, request, *args, **kwargs):
+    def post(self, request, format=None):
         data = request.data
         data['owner'] = request.user.id
         serializer = PostSerializer(data=data)
@@ -45,6 +41,9 @@ class PostList(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def get_queryset(self):
+        return Post.objects.filter(forum_id=self.kwargs['forum_id'])
 
 
 class PostDetail(RetrieveUpdateDestroyAPIView):
