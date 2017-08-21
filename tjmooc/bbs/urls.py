05e787aa0592b71593603b5984986693525e4cb3
dcalsky <dcalsky@163.com>
2017-08-21 23:03:10 +0800

    style(bbs): format url structure

--- tjmooc/bbs/urls.py
+++ tjmooc/bbs/urls.py
@@ -2,8 +2,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^forums$', views.ForumList.as_view()),
-    url(r'^forums/(?P<id>\w+)$', views.ForumDetail.as_view()),
-    url(r'^forums/(?P<forum_id>\w+)/posts$', views.PostList.as_view()),
-    url(r'^posts/(?P<id>\w+)$', views.PostDetail.as_view())
+    url(r'^forums/$', views.ForumList.as_view()),
+    url(r'^forums/(?P<id>\w+)/$', views.ForumDetail.as_view()),
+    url(r'^forums/(?P<forum_id>\w+)/posts/$', views.PostList.as_view()),
+    url(r'^posts/(?P<id>\w+)/$', views.PostDetail.as_view())
 ]
