167f9b2499ee428e8bd1ff3224abe42b761783be
dcalsky <dcalsky@163.com>
2017-08-23 11:50:28 +0800

    feat(backend session): update jwt response structure, add user information

--- tjmooc/session/views.py
+++ tjmooc/session/views.py
@@ -4,7 +4,8 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.permissions import AllowAny
 from .serializers import SessionSerializer
-from user.serializers import UserRegistrationSerializer
+from rest_framework.serializers import ModelSerializer
+from rest_framework_jwt.settings import api_settings
 
 User = get_user_model()
 
@@ -15,6 +16,35 @@ def jwt_response_payload_handler(token, user, request, *args, **kwargs):
         data[field] = user.__dict__.get(field, '')
     return data
 
+class DynamicFieldsModelSerializer(ModelSerializer):
+    def __init__(self, *args, **kwargs):
+        # Don't pass the 'fields' arg up to the superclass
+        fields = kwargs.pop('fields', None)
+
+        # Instantiate the superclass normally
+        super(DynamicFieldsModelSerializer, self).__init__(*args, **kwargs)
+
+        if fields is not None:
+            # Drop any fields that are not specified in the `fields` argument.
+            allowed = set(fields)
+            existing = set(self.fields.keys())
+            for field_name in existing - allowed:
+                self.fields.pop(field_name)
+
+
+class UserSerializer(DynamicFieldsModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'email')
+
+
+def jwt_response_payload_handler(token, user=None, request=None):
+    return {
+        'token': token,
+        'user': UserSerializer(user, context={'request': request}).data
+    }
+
+
 class UserLoginAPIView(GenericAPIView):
     permission_classes = (AllowAny,)
     serializer_class = SessionSerializer
@@ -22,19 +52,19 @@ class UserLoginAPIView(GenericAPIView):
     def post(self, request):
         serializer = SessionSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-        return Response(data=serializer.data,status=status.HTTP_200_OK)
+        return Response(data=serializer.data, status=status.HTTP_200_OK)
 
-    # def post(self, request, *args, **kwargs):
-    #     serializer = self.get_serializer(data=request.data)
-    #     if serializer.is_valid():
-    #         user = serializer.user
-    #         token, _ = Token.objects.get_or_create(user=user)
-    #         return Response(
-    #             data=TokenSerializer(token).data,
-    #             status=status.HTTP_200_OK,
-    #         )
-    #     else:
-    #         return Response(
-    #             data=serializer.errors,
-    #             status=status.HTTP_400_BAD_REQUEST,
-    #         )
+        # def post(self, request, *args, **kwargs):
+        #     serializer = self.get_serializer(data=request.data)
+        #     if serializer.is_valid():
+        #         user = serializer.user
+        #         token, _ = Token.objects.get_or_create(user=user)
+        #         return Response(
+        #             data=TokenSerializer(token).data,
+        #             status=status.HTTP_200_OK,
+        #         )
+        #     else:
+        #         return Response(
+        #             data=serializer.errors,
+        #             status=status.HTTP_400_BAD_REQUEST,
+        #         )
